b74c4994c2de59a5f5805da97f0bab86be5f8dc6
木子 <827183110@qq.com>
2021-03-30 22:05:52 +0800

    首页
    xx

--- app.js
+++ app.js
@@ -13,7 +13,27 @@ App({
         console.log(res)
       }
     })
-  },
+     // ---------------检测navbar高度
+    let menuButtonInfo = wx.getMenuButtonBoundingClientRect();
+    // console.log("小程序胶囊信息",menuButtonObject)
+     wx.getSystemInfo({
+       success: res => {
+         let statusBarHeight = res.statusBarHeight,
+           navTop = menuButtonInfo.top,//胶囊按钮与顶部的距离
+           navHeight = statusBarHeight + menuButtonInfo.height + (menuButtonInfo.top - statusBarHeight)*2;//导航高度
+         this.globalData.navHeight = navHeight;
+         this.globalData.navTop = navTop;
+         this.globalData.statusBarHeight=statusBarHeight;
+         this.globalData.menuButtonInfo=menuButtonInfo;
+         this.globalData.windowHeight = res.windowHeight;        
+         console.log("navHeight",navHeight);
+       },
+       fail(err) {
+         console.log(err);
+       }
+     })
+   },
+  
   globalData: {
     userInfo: null
   }
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,45 @@
 // pages/home/home.js
+
+const APP = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    imageUrl:[
+      "/images/lunbo/lunbotu2.png",
+      "/images/lunbo/lunbotu3.png",
+      "/images/lunbo/lunbotu6.jpg",
+      "/images/lunbo/lunbotu7.jpg"
+    ],
    swiperData:[],//轮播图
-   category:[],
- 
+   category:[
+     {id:1,categoryName:"书籍教材",categoryUrl:"/images/category/jiaocai_1.png"},
+     {id:2,categoryName:"电子教育",categoryUrl:"/images/category/dianzichanpin_1.png"},
+     {id:3,categoryName:"服装配饰",categoryUrl:"/images/category/wode-.png"},
+     {id:4,categoryName:"寝居必备",categoryUrl:"/images/category/shenghuoyongpin-copy.png"},
+     {id:5,categoryName:"文化体育",categoryUrl:"/images/category/tiyu.png"},
+     {id:6,categoryName:"其他",categoryUrl:"/images/category/qita.png"},
+     {id:7,categoryName:"文化体育",categoryUrl:"/images/category/tiyu.png"},
+     {id:8,categoryName:"其他",categoryUrl:"/images/category/qita.png"},
+     {id:9,categoryName:"文化体育",categoryUrl:"/images/category/tiyu.png"},
+     {id:10,categoryName:"其他",categoryUrl:"/images/category/qita.png"},
+    
+   ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.categoryLunBo(this.data.category)//轮播分组
+    if(this.data.categoryGroups.length>1){
+      this.setData({
+        autoPlay:true,
+        indicatorDots:true,
+      })
+    }
   },
 
   /**
@@ -27,8 +52,33 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow:function(){
+    console.log("App.globalData.navHeight", APP.globalData.navHeight)
+    console.log("App.globalData.navTop", APP.globalData.navTop)
+    console.log("App.globalData.windowHeight", APP.globalData.windowHeight)
+    let navHeight=APP.globalData.navHeight
+    let navTop=APP.globalData.navTop
+    let statusBarHeight=APP.globalData.statusBarHeight
+    console.log(statusBarHeight,"statusBarHeight")
+    let  menuButtonInfo=APP.globalData.menuButtonInfo
+    console.log(menuButtonInfo,"....")
+    let margin=navTop-statusBarHeight
 
+    let categoryGroupLenth=this.data.category.length
+    let categoryLunHeight= categoryGroupLenth>4? 2*70:60//动态计算种类轮播高度
+
+    this.setData({
+      navHeight:navHeight,
+      searchTop:navTop,
+      searchHeight:menuButtonInfo.height,
+      margin:margin,
+      searchWidth:menuButtonInfo.right-menuButtonInfo.width,
+      searchRight:menuButtonInfo.right,
+      categoryLunHeight:categoryLunHeight,
+      windowHeight: APP.globalData.windowHeight,   
+    })
+
+  
   },
 
   /**
@@ -64,5 +114,27 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  categoryLunBo:function(category){
+      if(category.length<=0){
+        return 
+      }
+      let categories=[].concat(category)
+      let categoryGroup=[];
+      let isbool=true
+      while(categories.length>0){
+        if(categories.length>=8){
+           categoryGroup.push(categories.splice(0,8))
+        }
+        else{
+          categoryGroup.push(categories.splice(0,categories.length))
+          break;
+        }
+      }
+      this.setData({
+        categoryGroups:categoryGroup,
+      })
+        console.log(categoryGroup)
+      },
+     
 })
